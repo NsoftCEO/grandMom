484f5dfee741b839a5aeb85b24841e19a402e779
user <user@10.10.2.25>
2026-03-21 16:42:57 +0900

    feat/next.js 설정 추가

--- src/main/java/ko/dh/goot/security/config/SecurityConfig.java
+++ src/main/java/ko/dh/goot/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ko.dh.goot.security.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import ko.dh.goot.security.handler.JwtAccessDeniedHandler;
 import ko.dh.goot.security.handler.JwtAuthenticationEntryPoint;
@@ -38,6 +43,7 @@ public class SecurityConfig {
     ) throws Exception {
        
     	http
+    		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authenticationProvider(authenticationProvider)
             .httpBasic(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
@@ -79,6 +85,21 @@ public class SecurityConfig {
             "/error",
             "/.well-known/**"
         );
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+
+        return source;
     }
     
     @Bean
